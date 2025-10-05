564a7a2109f1884d0a9b2803b96ada0b82c2020b
Matsumura Yuki <myuki.1722@gmail.com>
2025-10-05 20:14:10 +0900

    feat: Create a merker where click

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,15 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+"use client";
+
+import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import L, { LeafletMouseEvent } from "leaflet";
 
 // Leafletのデフォルトアイコンが正しく表示されない問題の修正
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,12 +21,31 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
+// マップクリックを検知
+function LocationMarker({
+  setNewPosition,
+}: {
+  setNewPosition: (position: L.LatLng) => void;
+}) {
+  useMapEvents({
+    click(e: LeafletMouseEvent) {
+      // クリック位置の座標をセット
+      setNewPosition(e.latlng);
+    },
+  });
+
+  return null;
+}
+
 export default function Map() {
-  const position: [number, number] = [35.6895, 139.6917]; // 初期位置を東京駅に設定
+  const initialPosition: [number, number] = [35.6895, 139.6917];
+
+  // 新しいピンの位置を保持
+  const [newPosition, setNewPosition] = useState<L.LatLng | null>(null);
 
   return (
     <MapContainer
-      center={position}
+      center={initialPosition}
       zoom={13}
       style={{ height: "100vh", width: "100wh" }}
     >
@@ -25,9 +53,16 @@ export default function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>ここは東京駅です</Popup>
-      </Marker>
+
+      {/* マップクリック検知コンポーネントを呼び出し */}
+      <LocationMarker setNewPosition={setNewPosition} />
+
+      {/* 新しいピンの位置が存在した場合、その場所にマーカーを表示 */}
+      {newPosition && (
+        <Marker position={newPosition}>
+          <Popup>新しい思い出の場所</Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 }
