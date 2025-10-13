c4522a559b1eadfb2b9946533852c4a5181e16e6
Matsumura Yuki <myuki.1722@gmail.com>
2025-10-13 16:19:36 +0900

    refactor(RealtimeLocationMarker): Apply Tailwind CSS

--- src/components/RealtimeLocationMarker.tsx
+++ src/components/RealtimeLocationMarker.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Marker } from "react-map-gl/mapbox";
-import styles from "./RealtimeLocationMarker.module.css";
 
 export default function RealtimeLocationMarker() {
   const [position, setPosition] = useState<{
@@ -31,7 +30,7 @@ export default function RealtimeLocationMarker() {
 
   return position === null ? null : (
     <Marker longitude={position.longitude} latitude={position.latitude}>
-      <div className={styles.locationMarker} />
+      <div className="w-5 h-5 bg-blue-500 rounded-full border-[3px] border-white shadow-lg" />
     </Marker>
   );
 }
